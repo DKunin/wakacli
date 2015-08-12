3cfc282a0f11a1440bcc3b2ba68712bec3877573
Dmitri Kunin <info@dkunin.ru>
2015-08-12 21:59:00 +0300

    Swapped request code for separate module

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 
 'use strict';
 
-var request  = require('superagent');
-var prettyMs = require('pretty-ms');
-var R        = require('ramda');
+var getHours = require('wakatoday');
 var nconf    = require('nconf');
 var Spinner  = require('cli-spinner').Spinner;
 var chalk    = require('chalk');
@@ -43,39 +41,11 @@ if(!apiKey) {
   throw new Error('You have to set api key before using, please consult wakatime --help');
 }
 
-var todaysDate = function(){
-  var d = new Date();
-  return (d.getMonth()+1)+'/'+d.getDate()+'/'+d.getFullYear();
-};
-
-var today = todaysDate();
-
-var summDuration = function(item, itemb){
-  return item.total_seconds + itemb.total_seconds;
-};
-
-var getFullDuration = R.compose(prettyMs, R.multiply(1000), R.reduce(summDuration, {total_seconds:0}), R.pluck('grand_total'),R.prop('data'));
-
-function getHours(){
-  return new Promise(function(resolve,reject){
-    spinner.start();
-    request
-      .get('https://wakatime.com/api/v1/summary/daily')
-      .query({start:today, end: today, api_key: apiKey})
-      .end(function (error, body) {
-        if(error) {
-          reject(error);
-          return;
-        }
-        resolve(getFullDuration(body.body));
-    }); 
-  });
-
-}
 
 function logInfo(data){
   spinner.stop(true);
   console.log(chalk.red(data));
 }
+spinner.start(true);
 
-getHours().then(logInfo).catch(logInfo);
+getHours(apiKey).then(logInfo).catch(logInfo);
